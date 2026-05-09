ce90fe0dd6dd302341bcb065c4c884d0c2ed0c09
xiaokui9869 <xiaokui9869@users.noreply.github.com>
2026-05-10 01:58:11 +0800

    更新应用版本到V2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.tools.overlay.xiaokui"
         minSdk = 36
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "V2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
